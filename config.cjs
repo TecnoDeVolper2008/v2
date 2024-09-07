545f04e3a0e006ed780210628b2530653351eb5a
SoloDeveloper-2008 <solodeveloper.lk@gmail.com>
2024-09-07 13:10:36 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Imalka-MD&MQZfzSML",
+  SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=nZ9w3CLL#THlE3itAjYLJEgWjHTCEVlRYSAvCEVt29DsyDR72TxUL",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
